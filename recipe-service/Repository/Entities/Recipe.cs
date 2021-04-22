91980eb2a49a1b5f2950b4ea9e5a1ce8042d65e9
floor van der schaft <i428231@fhict.nl>
2021-04-22 13:38:53 +0200

    Updated recipe entity

--- recipe-service/Repository/Entities/Recipe.cs
+++ recipe-service/Repository/Entities/Recipe.cs
@@ -5,5 +5,9 @@
         public int id { get; set; }
         public string title { get; set; }
         public string description { get; set; }
+        public int likes { get; set; }
+        public int preptime { get; set; }
+        public int serves { get; set; }
+        public int kcal { get; set; }
     }
 }
