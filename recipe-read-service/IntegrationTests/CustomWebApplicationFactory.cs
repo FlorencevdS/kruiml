0759dca9f228ba9507d983912ba2ae8fc449efd9
floor van der schaft <i428231@fhict.nl>
2021-05-27 16:13:17 +0200

    Added tests

---
+++ recipe-read-service/IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,48 @@
+﻿using IntegrationTests.Helpers;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Repository.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IntegrationTests
+{
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup: class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<RecipeContext>));
+                services.Remove(descriptor);
+                services.AddDbContext<RecipeContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                });
+
+                var sp = services.BuildServiceProvider();
+
+                using var scope = sp.CreateScope();
+                var scopedServices = scope.ServiceProvider;
+                var db = scopedServices.GetRequiredService<RecipeContext>();
+                var logger = scopedServices.GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+
+                db.Database.EnsureCreated();
+
+                try
+                {
+                    Utilities.InitializeDbForTests(db);
+                }
+                catch(Exception e)
+                {
+                    logger.LogError(e, "An error occured seeding the database with test data. Error: {Message}", e.Message);
+                }
+            });
+
+        }
+    }
+}
--- recipe-read-service/IntegrationTests/RecipeControllerIntegrationTest.cs
+++ recipe-read-service/IntegrationTests/RecipeControllerIntegrationTest.cs
@@ -1,18 +1,47 @@
-using System.Net;
+using Newtonsoft.Json;
+using Repository.Entities;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Text.Json;
 using Xunit;
 
 namespace IntegrationTests
 {
-    public class RecipeControllerIntegrationTest
+    public class RecipeControllerIntegrationTest : IClassFixture<CustomWebApplicationFactory<recipe_read_service.Startup>>
     {
-        [Fact]
-        public async void Test_Get_All()
-        {
-            var httpclient = new HttpClient();
-            var response = await httpclient.GetAsync("https://localhost:8081/recipe");
+        private readonly HttpClient _client;
+        private readonly CustomWebApplicationFactory<recipe_read_service.Startup> _factory;
 
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        public RecipeControllerIntegrationTest(CustomWebApplicationFactory<recipe_read_service.Startup> factory)
+        {
+            _factory = factory;
+            _client = _factory.CreateClient();
+        }
+
+        [Fact]
+        public async void Test_Get_All_Recipes()
+        {
+            var response = await _client.GetAsync("https://localhost:8081/recipe");
+            var content = await response.Content.ReadAsStringAsync();
+
+            var recipe = JsonConvert.DeserializeObject<List<Recipe>>(content);
+
+            response.EnsureSuccessStatusCode();
+            Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType.ToString());
+            Assert.Equal("Hummus", recipe[0].Title);
+        }
+
+        [Fact]
+        public async void Test_Get_Recipe_By_Id()
+        {
+            var response = await _client.GetAsync("https://localhost:8081/recipe/1");
+            var content = await response.Content.ReadAsStringAsync();
+
+            var recipe = JsonConvert.DeserializeObject<Recipe>(content);
+
+            response.EnsureSuccessStatusCode();
+            Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType.ToString());
+            Assert.Equal("Hummus", recipe.Title);
         }
     }
 }
